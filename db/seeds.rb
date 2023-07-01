1075bb725b2885308e45047c95337cc690b059f1
Kyle Nichols <kyle.r.nichols@gmail.com>
2023-07-01 14:31:05 +0900

    Rails wikinimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Destroying previous articles"
+Article.destroy_all
+
+puts "Creating fake articles"
+10.times do
+  article = Article.create!(
+    title: Faker::Lorem.sentence,
+    content: Faker::Lorem.paragraph_by_chars
+  )
+end
+
+puts "Done!"
